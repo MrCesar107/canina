7dafde2c480159ba372862b84be959b8d545c766
Cesar Rodriguez <cesar.rodriguez.lara54@gmail.com>
2018-12-14 00:20:16 -0600

    Se ha creado la logica de la aplicacion para calcular la edad canina

--- app/src/main/java/com/example/cesar/canina/MainActivity.java
+++ app/src/main/java/com/example/cesar/canina/MainActivity.java
@@ -3,25 +3,43 @@ package com.example.cesar.canina;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView answerText;
-    ImageView dogImage;
+    Toast toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        answerText = findViewById(R.id.answer_text);
+        final TextView answerText = findViewById(R.id.answer_text);
+        final EditText ageEditText = findViewById(R.id.age_edit_text);
         answerText.setText("Si fus perro tu edad seria de:");
 
-        dogImage = findViewById(R.id.dog_image);
+        ImageView dogImage = findViewById(R.id.dog_image);
         dogImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.dog));
 
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String ageString = ageEditText.getText().toString();
+                    int ageInt = Integer.parseInt(ageString);
+                    answerText.setText("Si fueras un perro, tu edad sería de: " + ( ageInt * 6 ) + " años");
 
+                } catch (NumberFormatException e) {
+                    toast.makeText(MainActivity.this, "Escribe un número válido", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
     }
 }
