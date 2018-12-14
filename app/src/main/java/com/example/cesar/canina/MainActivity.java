8cd6ce85543933a4df26df85f543e87c28091b91
Cesar Rodriguez <cesar.rodriguez.lara54@gmail.com>
2018-12-14 00:28:09 -0600

    Se han agregado Logs a la aplicacion

--- app/src/main/java/com/example/cesar/canina/MainActivity.java
+++ app/src/main/java/com/example/cesar/canina/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.cesar.canina;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,7 +12,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MyActivity";
     Toast toast;
 
     @Override
@@ -26,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         ImageView dogImage = findViewById(R.id.dog_image);
         dogImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.dog));
 
+        Log.d(TAG, "La app fue creada");
+
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,10 +36,13 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     String ageString = ageEditText.getText().toString();
                     int ageInt = Integer.parseInt(ageString);
+                    Log.i(TAG, "Se insertó un número entero");
                     answerText.setText("Si fueras un perro, tu edad sería de: " + ( ageInt * 6 ) + " años");
+
 
                 } catch (NumberFormatException e) {
                     toast.makeText(MainActivity.this, "Escribe un número válido", Toast.LENGTH_LONG).show();
+                    Log.e(TAG, "No se insertó un entero");
                 }
 
             }
